8fd1cb95863ca08f43b463aa5bd5665a58d98dc2
sadhvij19 <115769589+sadhvij19@users.noreply.github.com>
2023-04-18 10:21:02 +0530

    Update subtraction.js

--- subtraction.js
+++ subtraction.js
@@ -1,1 +1,31 @@
-html
+function subtractFractions(num1, den1, num2, den2) {
+ 
+  const commonDenominator = den1 * den2;
+
+  
+  const newNum1 = num1 * den2;
+  const newNum2 = num2 * den1;
+
+ 
+  const diffNumerators = newNum1 - newNum2;
+
+  
+  const gcd = findGCD(diffNumerators, commonDenominator);
+  const resultNum = diffNumerators / gcd;
+  const resultDen = commonDenominator / gcd;
+
+  
+  return `${resultNum}/${resultDen}`;
+}
+
+function findGCD(a, b) {
+  // Find the greatest common divisor (GCD) of two numbers
+  if (b === 0) {
+    return a;
+  } else {
+    return findGCD(b, a % b);
+  }
+}
+
+
+console.log(subtractFractions(3, 4, 1, 2)); // Output: 5/4
